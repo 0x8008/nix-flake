db31bee758f1d54e70c3b60e3a79345baf65f5dd
0x8008 <balu@xdapierdolnik.pl>
2026-04-03 10:39:16 +0200

    feat: enhance gaming configuration with kernel optimizations, udev rules, and additional utility packages

--- modules/system/gaming.nix
+++ modules/system/gaming.nix
@@ -1,19 +1,34 @@
 { config, pkgs, ... }:
 
 {
+  boot.kernelModules = [ "ntsync" ];
+  boot.kernel.sysctl = {
+    "kernel.split_lock_mitigate" = 0;
+  };
+
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     gamescopeSession.enable = true;
+    localNetworkGameTransfers.openFirewall = true;
   };
 
   programs.gamemode.enable = true;
-  programs.gamescope.enable = true;
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
+
+  services.udev.packages = [ pkgs.game-devices-udev-rules ];
 
   environment.systemPackages = with pkgs; [
     mangohud
     gamescope
     steam-run
     protonup-qt
+    winetricks
+    protontricks
+    mcpelauncher-ui-qt
+    (prismlauncher.override { jdks = [ jdk8 jdk21 graalvm-ce ]; })
   ];
 }
