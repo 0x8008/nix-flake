b5a061abc68ca16dc3954f503b2b0108bca74c95
0x8008 <balu@xdapierdolnik.pl>
2026-05-13 22:54:25 +0200

    feat: add DroidCam audio loopback configuration and patch tjTransform heap corruption

--- modules/system/video.nix
+++ modules/system/video.nix
@@ -1,11 +1,21 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
+  nixpkgs.overlays = [
+    (final: prev: {
+      droidcam = prev.droidcam.overrideAttrs (old: {
+        patches = (old.patches or []) ++ [
+          ../../packages/droidcam/fix-tjTransform-heap-corruption.patch
+        ];
+      });
+    })
+  ];
+
   programs.droidcam.enable = true;
 
   boot.kernelModules = [ "snd-aloop" ];
   boot.extraModprobeConfig = ''
     options v4l2loopback exclusive_caps=1 card_label="DroidCam"
-    options snd-aloop enable=1 index=7
+    options snd-aloop enable=1
   '';
 }
