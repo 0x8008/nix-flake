1582d0bfa498bc0617f7110aa2d83437f4326745
0x8008 <balu@xdapierdolnik.pl>
2026-05-13 18:53:40 +0200

    feat: implement libvirt virtualization with custom QEMU obfuscation patches and GPU passthrough support

---
+++ modules/system/virtualisation.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+
+{
+  programs.virt-manager.enable = true;
+
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = true;
+        swtpm.enable = true;
+        ovmf = {
+          enable = true;
+          packages = [ pkgs.OVMFFull.fd ];
+        };
+      };
+    };
+    spiceUSBRedirection.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    spice
+    spice-gtk
+    spice-protocol
+    virt-viewer
+    virtio-win
+    win-spice
+  ];
+}
